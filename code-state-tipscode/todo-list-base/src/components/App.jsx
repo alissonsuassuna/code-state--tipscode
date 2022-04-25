cb7543822526f19108601bd24b266e1475f88333
alissonsuassuna <alissonsuassuna@gmail.com>
2022-04-25 11:17:46 -0300

    code base - todo list

--- code-state-tipscode/todo-list-base/src/components/App.jsx
+++ code-state-tipscode/todo-list-base/src/components/App.jsx
@@ -2,35 +2,20 @@ import React, {useState} from "react";
 
 function App() {
 
-  const [inputText, setInputText] = useState('');
-  const [items, setItems] = useState([])
-
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
-
-  function addItem() {
-    setItems( (prevItems) => {
-      return [...prevItems, inputText]
-    } );
-    setInputText('');
-  }
-
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
+        <input type="text" />
+        <button >
           <span>Adicionar</span>
         </button>
       </div>
       <div>
         <ul>
-          {items.map( (todoItem) => <li>{todoItem}</li>) }
+         <li>Jesus te ama muito</li>
         </ul>
       </div>
     </div>
